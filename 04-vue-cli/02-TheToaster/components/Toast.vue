3c48d148c76eb122420c99f41d846774cb488941
feinth <rudeboyzzz666@gmail.com>
2023-02-05 22:23:17 +1000

    промежуточный

---
+++ 04-vue-cli/02-TheToaster/components/Toast.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="toast" :class="toast.toastClass">
+    <ui-icon class="toast__icon" :icon="toast.iconName" />
+    <span >{{ toast.text }}</span>
+  </div>
+</template>
+
+<script>
+import UiIcon from './UiIcon.vue';
+
+export default {
+  props: {
+    toast: {
+      type:Object,
+      required: true,
+    }
+  },
+  name: 'Toast',
+  components: { UiIcon },
+
+};
+</script>
+
+<style>
+</style>
